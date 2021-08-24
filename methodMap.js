944a9177b2e9551c972d8e7a326821e0532bcf20
rizkyputra <rizkyagrasyahputra19@gmail.com>
2021-08-24 16:10:22 +0700

    second commit

--- methodMap.js
+++ methodMap.js
@@ -1,5 +1,5 @@
 
-//calling a function each item in array
+// //calling a function each item in array
 const sweetArray = [2, 3, 4, 5, 35]
 const sweeterArray = sweetArray.map(sweetItem => {
     return sweetItem * 2
@@ -7,19 +7,19 @@ const sweeterArray = sweetArray.map(sweetItem => {
 
 console.log(sweeterArray)
 
-// create a function to use
+// // create a function to use
 const makeSweeter = sweetItem => sweetItem * 2;
 
-// we have an array
+// // we have an array
 const sweetArray = [2, 3, 4, 5, 35];
 
-// call the function we made. more readable
+// // call the function we made. more readable
 const sweeterArray = sweetArray.map(makeSweeter);
 
 console.log(sweeterArray);
 
 
-//Converting string to an array
+// //Converting string to an array
 const name = "Sammy"
 const map = Array.prototype.map
 
@@ -30,7 +30,7 @@ const newName = map.call(name, eachLetter => {
 console.log(newName)
 
 
-//Rendering list in js libraries
+// //Rendering list in js libraries
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -46,7 +46,7 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(<NamesList />, rootElement);
 
 
-//Reformatting array Objects
+// //Reformatting array Objects
 const myUsers = [
     { name: 'shark', likes: 'ocean' },
     { name: 'turtle', likes: 'pond' },
@@ -63,3 +63,61 @@ const usersByLikes = myUsers.map(item => {
 })
 
 console.log(usersByLikes);
+
+
+// Tugas 24/08/2021
+const daftarMobil = [
+  {
+   nama: 'Avanza',
+   brand: 'Toyota',
+   kecepatan: '120km/h'
+  },
+  {
+   nama: 'Xenia',
+   brand: 'Daihatsu',
+   kecepatan: '140km/h'
+  },
+  {
+   nama: 'Alphard',
+   brand: 'Toyota',
+   kecepatan: '180km/h'
+  }
+  ]
+  
+  function getSummary (a, b, c) {
+   return `Mobil ${b} ${a}, kecepatan maximal ${c} `
+  }
+  
+  let newArray = []
+  
+  daftarMobil.map((data) => {
+   newArray = newArray + getSummary(data.nama, data.brand, data.kecepatan)
+  })
+  
+  console.log(newArray);
+
+  let sortKecepatan = []
+
+  daftarMobil.map((data) => {
+  sortKecepatan.push(data.kecepatan)
+  })
+  
+  console.log(sortKecepatan)
+  
+  sortKecepatan.sort((a, b) => {return a - b})
+  
+  console.log(sortKecepatan);
+
+  
+  // Tugas 24/08/2021
+  const a = "rizky"
+
+  if(typeof a == 'number') {
+   console.log('Angka Bilangan bulat')
+  } else if (typeof a == 'string'){
+   console.log('String')
+  } else {
+   console.log('Boolean')
+  }
+
+  console.log(a);
