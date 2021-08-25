093d0e52a931336dc666e48352eae933d6a89016
rizkyputra <rizkyagrasyahputra19@gmail.com>
2021-08-25 21:46:25 +0700

    update soal nomor 3

--- tugasOH.js
+++ tugasOH.js
@@ -35,18 +35,21 @@
 
 
 // 2. Write a function that combines two lists by alternatingly taking elements, e.g. ['a', 'b', 'c', 'd', 'e', 'f'], [1, 2, 3] → [a,1,b,2,c,3].
-function combine(arr1, arr2) {
-  let merge = [];
-  let totalArr = arr1.length + arr2.length
+// function combine(arr1, arr2) {
+//   let merge = [];
+//   let totalArr = arr1.length + arr2.length
 
-  for(i=0; i < totalArr; i++){
-      merge.push(arr1[i], arr2[i])
-    }
-    return merge.filter((val) => val)
-  }
+//   for(i=0; i < totalArr; i++){
+//       merge.push(arr1[i], arr2[i])
+//     }
+//     return merge.filter((val) => val)
+//   }
 
-console.log(combine(['a', 'b', 'c', 'd', 'e', 'f'], [1, 2, 3]));
+// console.log(combine(['a', 'b', 'c', 'd', 'e', 'f'], [1, 2, 3]));
 
 // 3. Write a function that merges two sorted lists into a new sorted list. [1,4,6],[2,3,5] → [1,2,3,4,5,6]. You can do this quicker than concatenating them followed by a sort.
+const array1 = [1,4,6];
+const array2 = [2,3,5];
 
+console.log(array1.concat(array2).sort());
 // 4. Write function that translates a text to Pig Latin and back. English is translated to Pig Latin by taking the first letter of every word, moving it to the end of the word and adding 'ay'. "The quick brown fox" becomes "Hetay uickqay rownbay oxfay".
